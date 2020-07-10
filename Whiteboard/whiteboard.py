ef4943772edd869f18bb1709033c62923fa311a6
Carl Redding <credding07@gmail.com>
2020-07-10 18:35:10 -0400

    added whiteboard folder/file again so i could play around with some recursion functions. Finally got the binary search recursion working. not returning anything on my base case will stop you in your tracks lol.

---
+++ Whiteboard/whiteboard.py
@@ -0,0 +1,18 @@
+# # Recursion practice.
+# # counting down from n
+# def print_to_zero(n):
+#     if n < 0: 
+#         return 
+#     print(n)
+#     #if not
+#     return print_to_zero(n - 1) #calling the fn and decrementing
+# print(print_to_zero(5))
+
+#counting up to n
+def print_to_n(n):
+    if n > 15: #base case. once it hits 15 it stops printing.
+        return
+    print(n)
+    #if not
+    return print_to_n(n + 1)
+print(print_to_n(0))
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,37 @@
+# arr = [-5,-3,-2,-1,6,8,9,7,15,55,56,78]
+# arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+# target = 15
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
     
+#base case
+    while end >= start:
+    # if end >= start:
+        middle_index = (start + end) // 2
+
+        if arr[middle_index] == target:
+            return middle_index
+        else:
+            if target < arr[middle_index]:
+                end = middle_index -1 
+            # print(end)
+            if target > arr[middle_index]:
+                start = middle_index + 1
+            # print(arr[middle_index])
+            # print(start)
+        #if not in the array. -1 is a no-go.
+    return -1
+    #closing case 
+    return middle_index[0] + binary_search(middle_index[1:])
+    # return binary_search(middle_index[1:])
+    # return binary_search(middle_index)
+    # print(middle_index[0])
+    # print(middle_index[1:])
+
+# print(binary_search(arr, -9, 0, len(arr)-1))
+# print(middle_index[0])
+# print(arr[middle_index])
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
