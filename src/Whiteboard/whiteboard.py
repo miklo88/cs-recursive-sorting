046413e90fe40ec3455ca75ed409ec7d0fe7a0ce
Carl Redding <credding07@gmail.com>
2020-07-11 07:37:43 -0400

    merge-sort needs to be completed with stretch as well.

--- src/Whiteboard/whiteboard.py
+++ src/Whiteboard/whiteboard.py
@@ -8,11 +8,14 @@
 #     return print_to_zero(n - 1) #calling the fn and decrementing
 # print(print_to_zero(5))
 
-#counting up to n
-def print_to_n(n):
-    if n > 15: #base case. once it hits 15 it stops printing.
-        return
-    print(n)
-    #if not
-    return print_to_n(n + 1)
-print(print_to_n(0))
+# #counting up to n
+# def print_to_n(n):
+#     if n > 15: #base case. once it hits 15 it stops printing.
+#         return
+#     print(n)
+#     #if not
+#     return print_to_n(n + 1)
+# print(print_to_n(0))
+
+#merge sort
+
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,12 +1,21 @@
+arrA = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# arr2 = []
+# arr3 = [2]
+arrB = [0, 1, 2, 3, 4, 5]
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
+    #combining to make a single array to sort
     elements = len(arrA) + len(arrB)
+    # print(elements) #returns len of 16
     merged_arr = [0] * elements
-
+    
     # Your code here
 
 
     return merged_arr
+    
+print(merge(arrA, arrB))
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
@@ -19,10 +28,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
