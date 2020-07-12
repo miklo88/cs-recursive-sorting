557943a97e755707f002613b3eb4a75f89e5629e
Carl Redding <credding07@gmail.com>
2020-07-12 12:59:04 -0400

    sweet cheeseus i feel like a dummy. the whole point of this merge sort I was missing was my mergesort fn to call my merge fun....

--- src/Whiteboard/whiteboard.py
+++ src/Whiteboard/whiteboard.py
@@ -20,17 +20,22 @@
 #merge sort
 ##MERGING
 # # the goal here is to divide an array down by half until one element remains.
-# a = [7,14,6,8,12,15,2,20]
-# b = [17,4,16,18,2,5,12,2]
+a = [7,14,6,8,12,15,2,20]
+b = [17,4,16,18,2,5,12,2]
+# def merge(left, right):
 def merge(left, right):
+    
+    
      #going to need two places to store the array. left and right
+    # if len(left) == 0:
+    #     return right
     if len(left) == 0:
         return right
-    
+    print(left)
     #if nothing in the second array then return left
     if len(right) == 0:
         return left
-
+    print(right)
     result = []
     index_left = index_right = 0
 
@@ -42,10 +47,14 @@ def merge(left, right):
         if left[index_left] <= right[index_right]:
             result.append(left[index_left])
             index_left += 1
+            # print(left)
+            # print(index_left)
         else:
             result.append(right[index_right])
             index_right += 1
-        
+            # print(right)
+            # print(index_right)
+   
     #if you reach the end of either array then you can add the remaining elements from the other array to 
     #the result and break the loop
         if index_right == len(right):
@@ -56,11 +65,11 @@ def merge(left, right):
             result += right[index_right:]
             break
     return result
-# print(merge_sort(a, b))
+# print(merge(a,b))
 
 # merge_sort(arr)
-array = [17,4,16,18,2,5,12,2]
-#merge sort
+
+# #merge sort
 def merge_sort(array):
     if len(array) < 2:
         return array
@@ -72,4 +81,6 @@ def merge_sort(array):
         right=merge_sort(array[midpoint:])
     )
 
+array = [17,4,16,18,2,5,12,2]
+# print(merge_sort(a,b))
 print(merge_sort(array))
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,29 +1,61 @@
 arrA = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
-# arr2 = []
-# arr3 = [2]
+# arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+# # arr2 = []
+# # arr3 = [2]
 arrB = [0, 1, 2, 3, 4, 5]
 
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     #combining to make a single array to sort
-    elements = len(arrA) + len(arrB)
-    # print(elements) #returns len of 16
-    merged_arr = [0] * elements
+    # elements = len(arrA) + len(arrB)
+    # # print(elements) #returns len of 16
+    # merged_arr = [0] * elements
     
     # Your code here
+    if len(arrA) == 0:
+        return arrB
+    # print(arrA)
+    if len(arrB) == 0:
+        return arrA
+    # print(arrB)
+    merged_arr = []
+    index_left = index_right = 0
 
+    while len(merged_arr) < len(arrA) + len(arrB):
+        if arrA[index_left] <= arrB[index_right]:
+            merged_arr.append(arrA[index_left])
+            index_left += 1
 
+        else:
+            merged_arr.append(arrB[index_right])
+            index_right += 1
+
+        if index_right == len(arrB):
+            merged_arr += arrA[index_left:]
+            break
+
+        if index_left == len(arrA):
+            merged_arr += arrB[index_right:]
+            break
     return merged_arr
-    
-print(merge(arrA, arrB))
+# print(merge(arrA,arrB))
 
-# TO-DO: implement the Merge Sort function below recursively
+
+# # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) < 2:
+        return arr
 
+    midpoint = len(arr) // 2
+    
 
-    return arr
-
+    return merge(
+        arrA = merge_sort(arr[:midpoint]),
+        arrB = merge_sort(arr[midpoint:])
+    )
+    
+print(merge_sort(merge(arrA,arrB)))
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
